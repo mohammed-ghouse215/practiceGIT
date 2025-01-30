e55a9ad838d747b6fa6a881d72b17e05eae839a3
ghouseMD <ghouse08215@gmail.com>
2025-01-30 13:18:27 +0530

    added new Bike class

---
+++ NewProject/GitDemo/src/Bike.java
@@ -0,0 +1,22 @@
+public class Bike {
+    private String name;
+    private int price;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public Bike(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+
+}

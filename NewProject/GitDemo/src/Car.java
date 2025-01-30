937db2036e7cde850ea30f499ab7f2ca5c33fc99
ghouseMD <ghouse08215@gmail.com>
2025-01-30 11:57:52 +0530

    new files added

---
+++ NewProject/GitDemo/src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    private String name;
+    private int price;
+    public Car(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    
+
+}
